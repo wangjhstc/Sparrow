24b11fafb8309a9d33701fe5f97154e121e45840
wangjh <wangjhstc@126.com>
2016-01-25 17:10:40 +0800

    init project

---
+++ sparrow/sparrow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sparrow
+//
+//  Created by microsparrow on 16/1/25.
+//  Copyright © 2016年 xkeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sparrow/sparrow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sparrow
+//
+//  Created by microsparrow on 16/1/25.
+//  Copyright © 2016年 xkeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
